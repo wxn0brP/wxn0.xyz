404b8d8a06b93a32dd9714e850d771db826712fe
wxn0brP <toja.filiper@gmail.com>
2026-01-03 23:25:12 +0100

    feat: v0.0.5

--- src/game.ts
+++ src/game.ts
@@ -1,6 +1,6 @@
 import { decrementCell, incrementCell } from "@wxn0brp/flanker-ui/storeUtils";
 import { loadGame, saveGame } from "./save";
-import { print } from "./ui";
+import { input, print } from "./ui";
 import { $store, hackingMission, links, targets, vulnerabilities, xpToNextLevel } from "./vars";
 import { delay } from "@wxn0brp/flanker-ui/utils";
 
@@ -108,11 +108,24 @@ export function showLinks() {
     }
 }
 
-export function welcome() {
-    print("Welcome to <span class='system'>wxn0.xyz</span>", "system");
-    print("This is a mini-game to discover the ecosystem.");
-    print("Version 0.0.4.");
-    print("Type 'help' to see available commands.");
+export async function welcome() {
+    input.disabled = true;
+    print("CONNECTION ESTABLISHED.", "success");
+    await delay(600);
+    print("CRITICAL: SYSTEM BREACH DETECTED...", "error");
+    await delay(800);
+    print("BYPASSING SECURITY PROTOCOLS...", "system");
+    await delay(1200);
+    print("ACCESS GRANTED.", "success");
+    await delay(500);
+    print("----------------------------------------", "dim");
+    print("Welcome to <span class='system'>wxn0.xyz</span> Terminal Interface", "system");
+    print("Kernel v2.0.4-build.99 loaded.");
+    print("System Shell v0.0.5 loaded.")
+    print("----------------------------------------", "dim");
+    await delay(300);
+    print("Type '<span class='success'>help</span>' to list available commands.");
+    input.disabled = false;
 }
 
 loadGame();
