4902403a5352447feb695c00c45d13a7ccd1c787
wxn0brP <wxn0brP@users.noreply.github.com>
2026-01-04 13:52:01 +0000

    deploy: 3871cbe1c0ddd202424bd11a9b4f732cde0477d6

--- dist/shake.js
+++ dist/shake.js
@@ -188,3 +188,14 @@ if (localStorage.getItem("notHappened")) {
 } else {
   loadTerminal();
 }
+function goFullscreen() {
+  const elem = document.documentElement;
+  if (elem.requestFullscreen) {
+    elem.requestFullscreen();
+  } else if (elem.webkitRequestFullscreen) {
+    elem.webkitRequestFullscreen();
+  } else if (elem.msRequestFullscreen) {
+    elem.msRequestFullscreen();
+  }
+}
+document.addEventListener("click", goFullscreen, { once: true });
