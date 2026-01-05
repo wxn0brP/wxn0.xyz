333ee6c6f7ab764d001ffe788622ff1a470c451a
wxn0brP <toja.filiper@gmail.com>
2026-01-05 21:08:29 +0100

    feat: improve mail

--- src/commands/story.ts
+++ src/commands/story.ts
@@ -22,30 +22,31 @@ export function cmdMail(args: string[]) {
         return;
     }
 
-    if (args[0] === "read") {
-        const index = parseInt(args[1]) - 1;
-        if (isNaN(index) || index < 0 || index >= mails.length) {
-            print("Invalid message number.", "error");
-            return;
-        }
-
-        const mail = mails[index];
-        print("----------------------------------------", "dim");
-        print(`From: <span class="success">${mail.from}</span>`);
-        print(`Subject: ${mail.subject}`);
-        print(`Date: ${new Date(mail.timestamp).toLocaleString()}`);
-        print("----------------------------------------", "dim");
-        print(mail.body.replace(/\n/g, "<br>"));
-        print("----------------------------------------", "dim");
-
-        if (!mail.read) {
-            const newMails = [...mails];
-            newMails[index] = { ...mail, read: true };
-            $store.mails.set(newMails);
-            saveGame();
-        }
+    if (args[0] === "read") args.shift();
+    if (args.length === 0) {
+        print("Usage: mail read <number>", "error");
         return;
     }
 
-    print("Usage: mail [read <number>]", "error");
+    const index = parseInt(args[0]) - 1;
+    if (isNaN(index) || index < 0 || index >= mails.length) {
+        print("Invalid message number.", "error");
+        return;
+    }
+
+    const mail = mails[index];
+    print("----------------------------------------", "dim");
+    print(`From: <span class="success">${mail.from}</span>`);
+    print(`Subject: ${mail.subject}`);
+    print(`Date: ${new Date(mail.timestamp).toLocaleString()}`);
+    print("----------------------------------------", "dim");
+    print(mail.body.replace(/\n/g, "<br>"));
+    print("----------------------------------------", "dim");
+
+    if (!mail.read) {
+        const newMails = [...mails];
+        newMails[index] = { ...mail, read: true };
+        $store.mails.set(newMails);
+        saveGame();
+    }
 }
