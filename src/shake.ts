060597b11d263c57b3a402ff1a3e573b27b5a87f
wxn0brP <viviennevaltheris@gmail.com>
2026-09-12 15:42:46 +0200

    feat: mobile view

--- src/shake.ts
+++ src/shake.ts
@@ -130,4 +130,19 @@ if (localStorage.getItem("run")) {
 	if (Date.now() - run < day) loadTerminal();
 }
 
+const isMobile =
+	/Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(
+		navigator.userAgent,
+	) || window.innerWidth < 768;
+
+if (isMobile) {
+	document.body.innerHTML = `
+		<div id="mobile-page">
+			<h1>wxn0.xyz</h1>
+			<p>Don't look at me.</p>
+			<a href="https://github.com/wxn0brP">GitHub</a>
+		</div>
+	`;
+}
+
 export {};
